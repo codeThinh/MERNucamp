7d327564431964d942f0ba39885b1a4b1bd44256
codeThinh <codethinh@gmail.com>
2022-06-20 20:13:29 -0700

    express gen rest api

--- routes/index2.js
+++ routes/index2.js
@@ -1,10 +1,5 @@
 const router = require("express").Router();
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
-});
-
 router.use("/campsites", require("./campsiteRouter"));
 router.use("/promotions", require("./promotionRouter"));
 router.use("/partners", require("./partnerRouter"));
