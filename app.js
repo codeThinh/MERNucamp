d3f0e98c34f430e258a2b23187e9876d600db9b1
codeThinh <codethinh@gmail.com>
2022-06-25 08:45:05 -0700

    crud backend

--- app.js
+++ app.js
@@ -9,6 +9,21 @@ const usersRouter = require("./routes/users");
 
 const app = express();
 
+const mongoose = require("mongoose");
+
+const url = "mongodb://localhost:27017/nucampsite";
+const connect = mongoose.connect(url, {
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+connect.then(
+  () => console.log("Connected correctly to server"),
+  (err) => console.log(err)
+);
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
